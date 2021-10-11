60abb6a853eb240753331b8676e418c36412c95d
Santi Cebrian <scebrian@ossian.tech>
2021-10-11 17:31:00 +0200

    cerrar sesion

--- changelogsFRONT/src/app/components/home/home.component.ts
+++ changelogsFRONT/src/app/components/home/home.component.ts
@@ -1,24 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
 import { UserService } from '../../services/user.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, DoCheck {
 
   public identity: any;
-  public token: string; 
+  public token: any; 
 
   constructor(
-    private _userService: UserService
+    private _userService: UserService,
+    private _router: Router,
+    private _route: ActivatedRoute
   ) { 
     this.identity = this._userService.getIdentity();
     this.token = this._userService.getToken();
   }
 
   ngOnInit(): void {
+    if(!this.identity || !this.token){
+      this._router.navigate(['/login']);
+    }
+  }
 
+  ngDoCheck(){
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
   }
 
 }
--- changelogsFRONT/src/app/components/navbar/navbar.component.ts
+++ changelogsFRONT/src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserService } from '../../services/user.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  public identity: any;
+  public token: any; 
+
+  constructor(
+    private _userService: UserService,
+    private _router: Router,
+    private _route: ActivatedRoute
+  ) { 
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
+  }
 
   ngOnInit(): void {
+  }
+
+  logout(){
+    localStorage.clear();
+    this.identity = null;
+    this.token = null;
+
+    this._router.navigate(['/login']);
   }
 
 }
